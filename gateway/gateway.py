a2bb7b84f7fa9cf28027853fa1079167ae604242
David-LETINAUD <51359189+David-LETINAUD@users.noreply.github.com>
2019-06-05 08:54:02 +0200

    Gestion d'erreurs et envoie d'acquittements

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -11,12 +11,13 @@ import MySQLdb as mysql# Database
 
 # https://www.quennec.fr/trucs-astuces/langages/python/python-utiliser-un-fichier-de-param%C3%A8tres
 
-MY_SERVER_ADDRESS="0"
+MY_SERVER_ADDRESS=0 # de 0 à 9
 
 config = configparser.RawConfigParser() # On créé un nouvel objet "config"
 config.read('config.cfg') # On lit le fichier de paramètres
 
 serialPort = serial_rx_tx.SerialPort()
+serialPort.Send("test\n")
 
 # Récupération CONFIG SERIAL
 port = config.get('SERIAL','port')
@@ -66,19 +67,47 @@ def Temp(msg):
  vbat = float(tab[3])
  values = ( capteur, temp, hum, vbat)
  print("capteur : {} temp : {} hum : {} vbat : {}".format(capteur, temp, hum, vbat))
- # Commande SQL 
+
+ # Si valeur improbable : trame corrompu rendre code erreur
+ if capteur < 0 or capteur > 255:
+  return 1
+ elif vbat < 0 or vbat > 10:
+  return 2
+ elif hum < 0 or hum > 100:
+  return 3
+ elif temp < -50 or vbat > 99:
+  return 4
+
+ # Commande SQL
  insert_db(temp_query,values)
+ # Pas d'erreur
+ return 0
+ 
  
 def Analyse_Trames(m):
- if m[0]==MY_SERVER_ADDRESS:
+ r = -1
+ capteur_type = m[1:3]
+ addr_capt = m[3]
+ if m[0]==str(MY_SERVER_ADDRESS):
+  print("Server addr")   
   #Pour capteur température "TH"
-  if m[1:3]=="TH":
-   Temp(m[3:])
+  if capteur_type=="TH":
+   r = Temp(m[3:])   
   # Pour autres capteurs
   # ...
-  else :
+  
+ if r==-1 :
    print(m)
-
+   
+ elif r == 0:
+  print("OK")
+  #Envoie d'un acquittement si recu et pas d'erreurs
+  serialPort.Send("{}{}{} A".format(addr_capt,capteur_type,MY_SERVER_ADDRESS))
+ elif r!=1:
+  #Si erreurs : demande de renvoie
+  print("erreur")
+  serialPort.Send("{}{}{} E".format(addr_capt,capteur_type,MY_SERVER_ADDRESS))
+ 
 
 # serial data callback function
 def OnReceiveSerialData(message):
@@ -95,7 +124,7 @@ print("START")
 while 1:
  #x=serialPort.readline()
  #print(x)
- time.sleep(10)
+ time.sleep(10) 
 
 
 
